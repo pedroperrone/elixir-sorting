b1978b336fc0143356de0e6b06648772ef3f61b1
Pedro Perrone <psperrone@hotmail.com>
2018-07-16 19:58:13 -0300

    Merge function refactoring to increase performance (#8)

--- lib/merge_sort/helper.ex
+++ lib/merge_sort/helper.ex
@@ -5,13 +5,11 @@ defmodule MergeSort.Helper do
 
   def merge([], not_empty_list), do: not_empty_list
 
-  def merge([first_head | first_tail], [second_head | second_tail]) do
-    if first_head <= second_head do
-      [first_head] ++ merge(first_tail, [second_head | second_tail])
-    else
-      [second_head] ++ merge([first_head | first_tail], second_tail)
-    end
+  def merge([head | tail], second_list) when head <= hd(second_list) do
+    [head | merge(tail, second_list)]
   end
+
+  def merge(first_list, [head | tail]), do: [head | merge(first_list, tail)]
 
   def divide_list(list) do
     Enum.split(list, trunc(length(list) / 2))
