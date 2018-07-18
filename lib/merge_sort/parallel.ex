227f65f8e5a7f44a1da64e184f99c438dc30df33
Pedro Perrone <psperrone@hotmail.com>
2018-07-18 14:47:40 -0300

    Add a parameter to controll amount of parallel recurtion calls (#11)

--- lib/merge_sort/parallel.ex
+++ lib/merge_sort/parallel.ex
@@ -2,19 +2,50 @@ defmodule MergeSort.Parallel do
   import MergeSort.Helper
   alias MergeSort.Linear, as: Linear
 
-  def perform([]), do: []
+  def perform(list, parallelism_level \\ 1)
 
-  def perform([single_element | []]), do: [single_element]
+  def perform([], _), do: []
+  def perform(list = [_], _), do: list
+  def perform(list, parallelism_level), do: perform(list, parallelism_level, 1)
 
-  def perform(list) do
+  defp perform([], _, _), do: []
+  defp perform(list = [_], _, _), do: list
+
+  defp perform(list, parallelism_level, current_level) do
     list
     |> divide_list
-    |> merge_sublists
+    |> merge_sublists(parallelism_level, current_level + 1)
   end
 
-  defp merge_sublists({first_list, second_list}) do
+  defp merge_sublists(
+         {first_list, second_list},
+         parallelism_level,
+         current_level
+       )
+       when parallelism_level < current_level do
     Parallel.task(fn -> Linear.perform(first_list) end, :first_list)
     Parallel.task(fn -> Linear.perform(second_list) end, :second_list)
+
+    merge(
+      Parallel.capture_task(:first_list),
+      Parallel.capture_task(:second_list)
+    )
+  end
+
+  defp merge_sublists(
+         {first_list, second_list},
+         parallelism_level,
+         current_level
+       ) do
+    Parallel.task(
+      fn -> perform(first_list, parallelism_level, current_level) end,
+      :first_list
+    )
+
+    Parallel.task(
+      fn -> perform(second_list, parallelism_level, current_level) end,
+      :second_list
+    )
 
     merge(
       Parallel.capture_task(:first_list),
